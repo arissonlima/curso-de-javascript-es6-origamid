54b1971a783dde2c280829d9dd8fb175e21f3fc6
Arisson Lima <arissonlima@gmail.com>
2023-02-08 17:42:41 -0300

    First commit + Class Content + Exercises

---
+++ 01-variaveis.js
@@ -0,0 +1,30 @@
+var nome = "Arisson";
+var idade = 34;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = preco * totalComprado;
+
+console.log(totalPreco);
+
+// Exercício
+// - - - - - - -
+
+// Declarar uma variável com o seu nome
+var meuNome = "Arisson";
+// Declarar uma variável com a sua idade
+var minhaIdade = 34;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+// Declarar 5 variáveis diferentes sem valores
+var linguagemDeProgramacao,
+  timePreferido,
+  escolaDeProgramacao,
+  nomeDoCachorro,
+  nomeDoGato;
