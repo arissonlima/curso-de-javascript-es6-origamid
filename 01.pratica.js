bec66b95d31d9a3777243dc12d1223b1a9677e52
Arisson Lima <arissonlima@gmail.com>
2023-02-09 11:38:08 -0300

    Practicing the concept of variables

---
+++ 01.pratica.js
@@ -0,0 +1,70 @@
+var time = "Palmeiras";
+var cidadeDoTime = "São Paulo";
+var venceuOUltimoJogo = true;
+
+// - - - - - - - - - - - - - -
+
+var precoDoProduto = 50;
+var qtdCompradas = 5;
+var totalPreco = precoDoProduto * qtdCompradas;
+console.log(totalPreco);
+
+// - - - - - - - - - - - - - -
+
+var time1 = "Palmeiras",
+  time2 = "Corinthians",
+  time3 = "São Paulo";
+
+// - - - - - - - - - - - - - -
+
+var meuEstado;
+console.log(meuEstado);
+// Retorna undefined
+
+// - - - - - - - - - - - - - -
+
+var nome = "Arisson";
+var Nome = "José";
+
+// - - - - - - - - - - - - - -
+
+var possuiCursoSuperior = true;
+
+// - - - - - - - - - - - - - -
+
+// console.log(favoriteMovie);
+// Retorna is not defined
+
+// - - - - - - - - - - - - - -
+
+console.log(nomeDaMae);
+var nomeDaMae = "Maria";
+// Vai retornar undefined
+
+// - - - - - - - - - - - - - -
+
+var movie = "Pulp Fiction";
+movie = "Scarface";
+console.log(movie);
+
+let serie = "How I Met Your Mother";
+serie = "Friends";
+console.log(serie);
+
+const movieOrSeriesIsFavorite = true;
+movieOrSeriesIsFavorite = false;
+console.log(movieOrSeriesIsFavorite);
+
+// - - - - - - - - - - - - - -
+
+// Declarar uma variável com o seu nome
+var myNameIs = "Arisson";
+// Declarar uma variável com a sua idade
+let myAge = 34;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var favoriteFood;
+// Atribuir valor a sua comida favorita
+favoriteFood = "beans";
+// Declarar 5 variáveis diferentes sem valores
+let team1, team2, team3, team4, team5;
