ed6df18e2f14fecee7253efcb926fba5a6ec74fa
Arisson Lima <arissonlima@gmail.com>
2023-02-20 15:11:24 -0300

    Aulas e prática sobre Objetos finalizada

--- 06-pratica.js
+++ 06-pratica.js
@@ -38,3 +38,17 @@ var dog = {
 };
 
 console.log(dog.latir("homem"));
+
+// nomeie 3 propriedades ou métodos de strings
+// fixed, lenght, slice;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+// addEventListener
+// appendChild
+// InnerHTML
+// id
+// class
+
+// busque na web um objeto (método) capaz de interagir com o clipboard,
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+// ClipboardJS
