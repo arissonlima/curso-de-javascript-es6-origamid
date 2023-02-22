3f619b30e525742367c6b08dc4a2ddf704eb270a
Arisson Lima <arissonlima@gmail.com>
2023-02-22 15:47:31 -0300

    Aula e prática sobre Escopo

---
+++ 10-escopo.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+function fraseEmIngles() {
+  frase = "Think outside the box";
+}
+
+fraseEmIngles();
+console.log(frase);
+
+if (true) {
+  const nome = "Arisson";
+  console.log(nome);
+}
+
+console.log(nome);
